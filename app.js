5a6b1f8430773ec166aca49431fd4579b13fccdb
Ekene <macbookpro@MacBookPros-MacBook-Pro.local>
2019-11-09 01:24:13 +0000

    init commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// app.js 
+const express = require('express')
+const sls = require('serverless-http') 
+const app = express()
+
+app.get('/', async (req, res, next) => { 
+  res.status(200).send('Hello Serverless!') 
+}) 
+
+module.exports.server = sls(app)
